8ce66d1ebf1f9e9c1231c59fdabccebca85e5630
Tafadzwa Musiyiwa <tnmusiyiwa@gmail.com>
2024-07-16 08:16:21 +0200

    Implement React Router

---
+++ src/components/CountryList.jsx
@@ -0,0 +1,33 @@
+/* eslint-disable react/prop-types */
+import styles from "./CountryList.module.css";
+import Spinner from "./Spinner";
+import Message from "./Message";
+import CountryItem from "./CountryItem";
+
+function CountryList({cities, isLoading}) {
+  if (isLoading) return <Spinner />;
+
+  if (!cities?.length)
+    return (
+      <Message message="Add your first city by clicking on a city on the map!" />
+    );
+
+  const countries = cities.reduce((arr, city) => {
+    3;
+    if (!arr.map((el) => el.country).includes(city.country)) {
+      return [...arr, {country: city.country, emoji: city.emoji}];
+    } else {
+      return arr;
+    }
+  }, []);
+
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem key={country.country} country={country} />
+      ))}
+    </ul>
+  );
+}
+
+export default CountryList;
